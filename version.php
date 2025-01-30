ca2cb0a5648df6f5d6ef66e616c6ce971a5947d6
rezeau <joseph@rezeau.org>
2025-01-30 18:22:40 +0100

    version = 2025013001

--- version.php
+++ version.php
@@ -30,7 +30,7 @@ $plugin->component = 'qtype_guessit';
 $plugin->dependencies = [
     'qbehaviour_guessit'   => 2025012600,
 ];
-$plugin->version = 2025013000;
+$plugin->version = 2025013001;
 $plugin->requires = 2022040100;  // Moodle 4.0.
 $plugin->release = '1.0';
 $plugin->supported = [401, 405];
